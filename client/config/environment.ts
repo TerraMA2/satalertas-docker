ba7b439c41cdff38e751d864337603bf8fc2cc3b
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-07-29 16:07:35 -0300

    Added project to environment

--- client/config/environment.ts
+++ client/config/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  project: '',
   terramaUrl: 'http://localhost/adm',
   reportServerUrl: 'http://localhost/alertaServer',
   geoserverUrl: 'http://localhost/geoserver'
