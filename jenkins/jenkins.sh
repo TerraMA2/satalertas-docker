a60c41bc08370382d5d492780bae36beb9398df1
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-08-16 00:31:01 -0300

    Added jenkins.sh script

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -4,4 +4,6 @@ cd jenkins || exit
 
 docker network create jenkins
 
+docker build -t jenkins/jenkins:jdk11 .
+
 docker run --name jenkins -d --privileged --restart=always -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) --network jenkins -p 36001:8080 jenkins/jenkins:jdk11
