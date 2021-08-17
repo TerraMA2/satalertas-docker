c90905bbcedf28c6bc21653956ebc7716c1942f6
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-08-17 00:09:23 -0300

    Refactoring

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-CURRENT_FOLDER=$(basename $(pwd))
-if [ ! "$CURRENT_FOLDER" == 'jenkins' ] ; then
-  cd jenkins || exit
-fi
+docker volume create jenkins_home
+docker network create admin_net
 
-docker network create jenkins
-
-docker run --name jenkins -d --privileged --restart=always -v jenkins_home:/var/jenkins_home --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v $(which docker):$(which docker) --network jenkins -p 36001:8080 jenkins/jenkins:jdk11
+docker run --name=jenkins -d --privileged --restart=always -v jenkins_home:/var/jenkins_home --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v $(which docker):$(which docker) --network admin_net -p 36062:8080 jenkins/jenkins:jdk11
--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker volume create portainer_data
+docker network create admin_net
 
-docker run -d -p 36010:9000 --name=portainer --restart=always --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:alpine -H unix:///var/run/docker.sock
+docker run --name=portainer -d --privileged --restart=always -p 36060:9000 --network admin_net --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:alpine
 
-docker run -d -p 36011:9001 --name portainer_agent --restart=always --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:alpine -H unix:///var/run/docker.sock
+#docker run -d -p 36061:9001 --name portainer_agent --restart=always --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:alpine
