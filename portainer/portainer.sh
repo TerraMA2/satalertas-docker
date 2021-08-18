6d027b450df71824a47c849cfa46e4bef30b7239
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-08-18 17:55:03 -0300

    Updated portainer's docker-compose.yml

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 docker volume create portainer_data
-docker network create admin_net
+docker network create --scope swarm --internal --attachable admin_net
 
-docker run --name=portainer -d --privileged --restart=always -p 36060:9000 --network admin_net --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:alpine
+#docker run --name=portainer -d \
+#            --restart=unless-stopped \
+#            --privileged \
+#            -p 36060:9000 \
+#            --network admin_net \
+#            --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+#            --mount type=bind,src=/var/lib/docker/volumes,dst=/var/lib/docker/volumes \
+#            -v portainer_data:/data portainer/portainer-ce:alpine
 
-#docker run -d -p 36061:9001 --name portainer_agent --restart=always --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:alpine
+docker service create --name=portainer -d \
+                      --restart-condition=on-failure \
+                      --restart-max-attempts=2 \
+                      --publish 36060:9000 \
+                      --network admin_net \
+                      --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+                      --mount type=bind,src=/var/lib/docker/volumes,dst=/var/lib/docker/volumes \
+                      --mount type=volume,src=portainer_data,dst=/data portainer/portainer-ce:alpine
