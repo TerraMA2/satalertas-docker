64fd0be08eccb81a8c5a1830d03d8c421de25f5b
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-08-24 11:32:51 -0300

    Backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
-eval $(egrep -v '^#' ../.env | xargs)
+BACKUP_DIR="/data/backup"
+
+usage() {
+  MESSAGE=$1
+  echo "Error: ${MESSAGE}"
+  echo ""
+  echo "Usage: backup [ -B | --backup-dir directory ]
+              [ -p | --project project ]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n argument -o B:p: --long backup-dir:,project: -- "$@")
+
+VALID_ARGUMENTS=$?
+
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage "No arguments passed"
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while :; do
+  case "$1" in
+  -B | --backup-dir)
+    BACKUP_DIR="$2"
+    shift 2
+    ;;
+  -p | --project)
+    COMPOSE_PROJECT_NAME="$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo "Opção $1 não reconhecida."
+    usage
+    ;;
+  esac
+done
+
+if [ ! "$BACKUP_DIR" ]; then
+  usage "Backup directory not specified"
+fi
+
+if [ ! "$COMPOSE_PROJECT_NAME" ]; then
+  usage "Project not specified"
+fi
 
 DATE=$(date +%d-%m-%Y-%H-%M)
 
@@ -11,9 +58,9 @@ echo "*********************"
 echo ""
 
 if test -d "/var/lib/docker/volumes/${COMPOSE_PROJECT_NAME}_satalertas_documents_vol/_data/"; then
-    mkdir -vp ${BACKUP_DIR}/satalertas
+    mkdir -vp ${BACKUP_DIR}/${COMPOSE_PROJECT_NAME}/satalertas
 
-    cd /var/lib/docker/volumes/${COMPOSE_PROJECT_NAME}_satalertas_documents_vol/_data/
+    cd /var/lib/docker/volumes/${COMPOSE_PROJECT_NAME}_satalertas_documents_vol/_data/ || exit
 
-    tar cvf - *.pdf | gzip -9 - > ${BACKUP_DIR}/satalertas/satalertas_documents-${DATE}.tar.gz
+    tar cvf - *.pdf | gzip -9 - > ${BACKUP_DIR}/${COMPOSE_PROJECT_NAME}/satalertas/satalertas_documents-${DATE}.tar.gz
 fi
--- backup/restore.sh
+++ backup/restore.sh
@@ -1,14 +1,61 @@
 #!/bin/bash
 
-eval $(egrep -v '^#' ../.env | xargs)
+BACKUP_DIR="/data/backup"
+
+usage() {
+  MESSAGE=$1
+  echo "Error: ${MESSAGE}"
+  echo ""
+  echo "Usage: restore [ -B | --backup-dir directory ]
+              [ -p | --project project ]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n argument -o B:p: --long backup-dir:,project: -- "$@")
+
+VALID_ARGUMENTS=$?
+
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage "No arguments passed"
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while :; do
+  case "$1" in
+  -B | --backup-dir)
+    BACKUP_DIR="$2"
+    shift 2
+    ;;
+  -p | --project)
+    COMPOSE_PROJECT_NAME="$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo "Opção $1 não reconhecida."
+    usage
+    ;;
+  esac
+done
+
+if [ ! "$BACKUP_DIR" ]; then
+  usage "Backup directory not specified"
+fi
+
+if [ ! "$COMPOSE_PROJECT_NAME" ]; then
+  usage "Project not specified"
+fi
 
 echo ""
-echo "********************"
+echo "**********************"
 echo "* Restore SatAlertas *"
-echo "********************"
+echo "**********************"
 
 if test -d "/var/lib/docker/volumes/${COMPOSE_PROJECT_NAME}_satalertas_documents_vol/_data/"; then
-    cd ${BACKUP_DIR}/satalertas
+    cd ${BACKUP_DIR}/${COMPOSE_PROJECT_NAME}/satalertas
 
     latestDocumentBackupFile=$(ls -t1 | head -n 1)
     
